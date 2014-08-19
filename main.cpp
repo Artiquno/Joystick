1e1ec602eee2ed234877a0003ce67e1517321f01
CodeRend <en31995@gmail.com>
2014-08-19 09:15:51 +0200

    I'm committing with my joystick!!!

--- main.cpp
+++ main.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
-#include <string>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Joystick.hpp>
+
+#define DIV 15
+#define SPEED 1
+#define INPUT_SLEEP 200
+
+#define getPos Joystick::getAxisPosition
+#define butPress Joystick::isButtonPressed
+
+//Note: L1 = Shift
+//      L2 = Ctrl
+//      R2 = Alt
 
 using namespace std;
 using namespace sf;
 
-bool holding = false;
+bool alt, shift, ctrl, holding = false;
 
 enum joyButton
 {
     Triangle = 0, Circle, Cross, Square,
-    Up, Right, Down, Left,
+    L1, R1,
+    L2, R2,
     Select, Start,
-    R1, R2,
-    L1, L2,
     AnaLeft, AnaRight
 };
 
 void update();
-void input(char cmd[], char R1[], char R2[], char L1[], char L2[], unsigned short int joystickId = 1);
+void input(char cmd[], char R1[], char R2[], char L1[], char L2[], unsigned short int joystickId = 0);
+char getJoystick(unsigned char connected);
 
 void update()
 {
@@ -28,7 +38,7 @@ void update()
 
 void input(char cmd[], char R1[], char R2[], char L1[], char L2[], unsigned short int joystickId)
 {
-    if(Joystick::isButtonPressed(joystickId, joyButton::R1))
+    if(butPress(joystickId, joyButton::R1))
     {
         if(!holding)
         {
@@ -37,70 +47,116 @@ void input(char cmd[], char R1[], char R2[], char L1[], char L2[], unsigned shor
         }
         else
         {
-            system("xdotool mouseup 1 mouseup 2 mouseup 3");
+            system("xdotool mouseup 1 mouseup 2 mouseup 3 keyup Super");
             holding = false;
         }
     }
-    //else if(Joystick::isButtonPressed(joystickId, joyButton::R2));
+    //else if(butPress(joystickId, joyButton::R2));
         //Do something
-    //else if(Joystick::isButtonPressed(joystickId, joyButton::L1));
+    //else if(butPress(joystickId, joyButton::L1));
         //Do something
-    //else if(Joystick::isButtonPressed(joystickId, joyButton::L2));
+    //else if(butPress(joystickId, joyButton::L2));
         //Do something
     else
         system(cmd);
+}
 
-    sleep(sf::milliseconds(200));
+char getJoystick()
+{
+    for(int i = 0; i < 10; ++i)
+    {
+        if(Joystick::isConnected(i))
+            return i;
+    }
+    return -1;
 }
 
 int main()
 {
-    RenderWindow wnd(VideoMode(200, 200), "Some random window!");
     bool running = true;
+    bool paused = false;
+    Joystick::update();
+    char connected = getJoystick();
+    if(connected == -1)
+    {
+        running = false;
+        cout << "Failed to detect any joystick!\n";
+    }
+    system("onboard");
     while(running)
     {
         Thread updater(update);
         updater.launch();
-        //sleep(milliseconds(1000/30));
-        if(Keyboard::isKeyPressed(Keyboard::Return) || Joystick::isButtonPressed(1, 9))
-            running = false;
-        else if(Joystick::getAxisPosition(1, Joystick::Axis::X) > 0)
+        sleep(milliseconds(1000/80));
+        if(!paused)
         {
-            Mouse::setPosition(Mouse::getPosition() + Vector2i(5, 0));
-        }
-        else if(Joystick::getAxisPosition(1, Joystick::Axis::Y) > 0)
-        {
-            Mouse::setPosition(Mouse::getPosition() + Vector2i(0, 5));
-        }
-        else if(Joystick::getAxisPosition(1, Joystick::Axis::X) < 0)
-        {
-            Mouse::setPosition(Mouse::getPosition() + Vector2i(-5, 0));
-        }
-        else if(Joystick::getAxisPosition(1, Joystick::Axis::Y) < 0)
-        {
-            Mouse::setPosition(Mouse::getPosition() + Vector2i(0, -5));
-        }
-        else if(Joystick::isButtonPressed(1, joyButton::Triangle))
-        {
-            input("xdotool key Return", "", "", "", "");
-        }
-        else if(Joystick::isButtonPressed(1, joyButton::Circle))
-        {
-            input("xdotool click 3", "xdotool mousedown 3", "", "", "");
-        }
-        else if(Joystick::isButtonPressed(1, joyButton::Cross))
-        {
-            input("xdotool click 1", "xdotool mousedown 1", "", "", "");
-        }
-        else if(Joystick::isButtonPressed(1, joyButton::Square))
-        {
-            input("xdotool click 2", "xdotool mousedown 2", "", "", "");
-        }
+//            if(butPress(connected, joyButton::Select))
+//                running = false;
+//            else
+            if(butPress(connected, joyButton::Triangle))
+            {
+                input("xdotool key BackSpace", "", "", "", "", connected);
+                sleep(sf::milliseconds(INPUT_SLEEP));
+            }
+            else if(butPress(connected, joyButton::Circle))
+            {
+                input("xdotool click 3", "xdotool mousedown 3", "", "", "", connected);
+                sleep(sf::milliseconds(INPUT_SLEEP));
+            }
+            else if(butPress(connected, joyButton::Cross))
+            {
+                input("xdotool click 1", "xdotool mousedown 1", "", "", "", connected);
+                sleep(sf::milliseconds(INPUT_SLEEP));
+            }
+            else if(butPress(connected, joyButton::Square))
+            {
+                input("xdotool click 2", "xdotool mousedown 2", "", "", "", connected);
+                sleep(sf::milliseconds(INPUT_SLEEP));
+            }
+            else if(butPress(connected, joyButton::Select))
+            {
+                input("xdotool key Super", "xdotool keydown Super", "", "", "", connected);
+                sleep(sf::milliseconds(INPUT_SLEEP));
+            }
+            else if(butPress(connected, joyButton::Start))
+            {
+                input("xdotool key Return", "xdotool keydown Return", "", "", "", connected);
+            }
+            else if(butPress(connected, joyButton::L1) &&
+                    butPress(connected, joyButton::R1))
+            {
+                paused = true;
+                cout << "Paused = true\n";
+                sleep(milliseconds(500));
+            }
 
-        wnd.setVisible(false);
+            if(Joystick::getAxisPosition(connected, Joystick::Axis::X) > 0)
+            {
+                Mouse::setPosition(Mouse::getPosition() + Vector2i(getPos(connected, Joystick::Axis::X)/DIV * SPEED, 0));
+            }
+            else if(Joystick::getAxisPosition(connected, Joystick::Axis::X) < 0)
+            {
+                Mouse::setPosition(Mouse::getPosition() + Vector2i(getPos(connected, Joystick::Axis::X)/DIV * SPEED, 0));
+            }
+
+            if(Joystick::getAxisPosition(connected, Joystick::Axis::Y) > 0)
+            {
+                Mouse::setPosition(Mouse::getPosition() + Vector2i(0, getPos(connected, Joystick::Axis::Y)/DIV * SPEED));
+            }
+            else if(Joystick::getAxisPosition(connected, Joystick::Axis::Y) < 0)
+            {
+                Mouse::setPosition(Mouse::getPosition() + Vector2i(0, getPos(connected, Joystick::Axis::Y)/DIV * SPEED));
+            }
+        }
+        if( butPress(connected, joyButton::R1) &&
+            butPress(connected, joyButton::R2))
+        {
+            paused = false;
+            cout << "Paused = false\n";
+            sleep(milliseconds(500));
+        }
     }
     //cin.ignore();
-    wnd.close();
     cout << "Goodbye!\n";
     return 0;
 }
